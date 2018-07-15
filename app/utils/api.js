dfaf5643fb91152583fa162d9fa6eededc38465a
khusroo hayat <khusroo.hayat@gmail.com>
2018-07-16 01:15:53 +0500

    axios, api updated

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,6 +1,65 @@
 import axios from "../../node_modules/axios";
 
+var id= '0c86b4bddaae3f2268c2'//"YOUR_CLIENT_ID";
+var sec= '8d05e5c1a576d6329a83cabf5616e9ba56e3b047'//"Your_SECRET_ID";
+var params = "?client_id=" + id + "&client_secret=" + sec;
+
+function getProfile (username) {
+    return axios.get('https://api.github.com/users/' + username + params)
+        .then(function (user) {
+            return user.data;
+        })
+}
+
+function getRepos (username) {
+    return axios.get('https://api.github.com/users/' + username + '/repos' + params + '&per_page=100')
+}
+
+function getStarCount (repos) {
+    return repos.data.reduce(function (count, repo) {
+        return count + repo.stargazers_count
+    }, 0)
+}
+
+function calculateScore (profile, repos) {
+    const followers = propfile.followers
+    const totalStars = getStarCount(repos)
+
+    return (followers * 3) + totalStars
+}
+
+function handleError (error) {
+    console.warn(error)
+    return null
+}
+
+function getUserData(player) {
+    return axios.all([
+        getProfile(player),
+        getRepos(player)
+    ]).then(function (data){
+        const profile = data[0]
+        const repos = data[1]
+
+        return {
+            profile:profile,
+            score: calculateScore(profile, repos)
+        }
+    })
+}
+
+function sortPlayers(players) {
+    return players.sort(function (a, b) {
+        return b.score - a.score
+    })
+}
+
 module.exports = {
+    battle: function (players) {
+        return axios.all(players.map(getUserData))
+            .then(sortPlayers)
+            .catch(handleError);
+    },
     fetchPopularRepos: function (language) {
         var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
 
